d7004770850987170b8c9dfcb756368c49f10393
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2021-12-31 18:30:14 +0700

    no.16 fixing comment

--- ploting/no_16.py
+++ ploting/no_16.py
@@ -45,14 +45,14 @@ def sorting(col_data):
 def bbs():
     n = len(col_data)
 
-    # Traverse through all col_dataay elements
+    # Traverse through all array elements
     for i in range(n - 1):
         # range(n) also work but outer loop will repeat one time more than needed.
 
         # Last i elements are already in place
         for j in range(0, n - i - 1):
 
-            # traverse the col_dataay from 0 to n-i-1
+            # traverse the array from 0 to n-i-1
             # Swap if the element found is greater
             # than the next element
 
