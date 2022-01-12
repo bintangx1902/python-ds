71a6939d5c1451ad7cf80ccb9832613032283195
bintangx1902 <=>
2022-01-12 22:38:41 +0700

    np slice and addition

--- DS-path/basic/day_2.py
+++ DS-path/basic/day_2.py
@@ -24,9 +24,24 @@ bmi = [weight_kg[x] / height_m[x]**2 for x in range(len(weight_lb)-1)]
 weight_kg = np.array(weight_lb) * .453592
 height_m = np.array(height_in) * .0254
 bmi = weight_kg / height_m ** 2
+""" you can slice the numpy arrays like this """
+sliced = bmi[100:111]
 
 # print(f"{weight_kg} \n{height_m} \n{bmi}")
 
 """ this provide how to filter the bmi score below 21 """
 lite = bmi < 21
 print(bmi[lite])
+
+
+""" addition section """
+a = list(range(2, 23)) + [False, True, False]
+b = list(range(50, 71)) + [True, True, False]
+
+""" standard -> you must iter 1 by 1 index """
+c = [a[x] + b[x] for x in range(len(a))]
+# print(c)
+
+""" numpy logic is just like standard addition """
+c = np.array(a) + np.array(b)
+# print(c)
